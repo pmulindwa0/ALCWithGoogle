787b927d978f21918368cb3a5324a1609eace9af
pecode <pmulindwa0@gmail.com>
2018-07-01 13:22:35 +0300

    db storage

--- js/db.js
+++ js/db.js
@@ -1,38 +1,28 @@
-const customerData = [
-    { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
-    { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
-  ];
+const currencyAPIC = new CurrencyAPI();
+const uiC = new UI();
+var request = indexedDB.open("currencyDB");
+
+request.onupgradeneeded = function() {
   
-const dbName = "currencies";
+  var db = request.result;
+  var store = db.createObjectStore("currencies", {keyPath: "id"});
+  var nameIndex = store.createIndex("by_currencyName", "currencyName", {unique: true});
+  var symbolIndex = store.createIndex("by_symbol", "currencySymbol");
 
-var request = indexedDB.open(dbName, 2);
+  currencyAPIC.getCurrenciesList()
+    .then(data =>{
+        const results = data.results;
+        const currencies = Object.values(results);
+        // console.log(currencies);
+        
+        currencies.forEach(currency => {
 
-request.onerror = function(event) {
-  // Handle errors.
+          store.put(currency);
+        })
+    }) 
+
 };
-request.onupgradeneeded = function(event) {
-  var db = event.target.result;
 
-  // Create an objectStore to hold information about our customers. We're
-  // going to use "ssn" as our key path because it's guaranteed to be
-  // unique - or at least that's what I was told during the kickoff meeting.
-  var objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
-
-  // Create an index to search customers by name. We may have duplicates
-  // so we can't use a unique index.
-  objectStore.createIndex("name", "name", { unique: false });
-
-  // Create an index to search customers by email. We want to ensure that
-  // no two customers have the same email, so use a unique index.
-  objectStore.createIndex("email", "email", { unique: true });
-
-  // Use transaction oncomplete to make sure the objectStore creation is 
-  // finished before adding data into it.
-  objectStore.transaction.oncomplete = function(event) {
-    // Store values in the newly created objectStore.
-    var customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
-    customerData.forEach(function(customer) {
-      customerObjectStore.add(customer);
-    });
-  };
+request.onsuccess = function() {
+  db = request.result;
 };
